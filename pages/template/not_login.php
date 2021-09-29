ea77f51e5f6fd472e1172a7c4c674028a0e03faf
Ahmad Rivaldi <rivaldi19122@gmail.com>
2021-09-29 16:44:00 +0700

    add template not_login.php

---
+++ pages/template/not_login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="icon" href="../static/img/icon-404.png" type="image/icon">
+
+    <title>Access Denied</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row vh-100 d-flex justify-content-center align-items-center">
+            <div class="col-md-7 col-lg-5">
+                <h3 class="text-center">ACCESS FORBIDDEN | 403</h3>
+                <p class="tex-center">You will redirect in <span id="count-down"></span></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
