67023acfddffc3d37ca3574df054b9197e1c618e
Ahmad Rivaldi <rivaldi19122@gmail.com>
2021-09-30 21:34:51 +0700

    fix typo

--- pages/action/index.php
+++ pages/action/index.php
@@ -2,15 +2,24 @@
 
 <?php
 session_start();
+include("../../lib/function.php");
+
 
 $uid = $_GET['uid'];
-$acton = $_GET['act'];
+$action = $_GET['act'];
 $session_id = $_SESSION['session_id'];
 
 if (empty($session_id)) {
     include('../template/not_login.php');
+}else {
+    if ($action == 'd') {
+        
+        if(delete_user($uid)){
+            header("location:../admin/");
+        }else {
+            echo("<script>alert('failed to delete user')</script>");
+            header("location:../admin/");
+        }
+    }
 }
-
-
-
 ?>
